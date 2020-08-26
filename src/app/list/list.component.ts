41c51bf1da6cc0f23bcd3f65d08d2bc8879d9920
Saikrishna Naripeddi <saikrishna.naripeddi@Saikrishnas-MBP.hsd1.ga.comcast.net>
2020-08-26 12:56:25 -0400

    comments for the logic performed

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -29,12 +29,21 @@ export class ListComponent implements OnInit {
     }
   ];
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
+    // I have saved the values of the unselcted and selected providers in localStorage 
+    // and checking if the values are already present in localStorage and 
+    // I am resetting the values of both the unselected and selected Providers. 
+    // This will be helpful to restore the values of the unselected and selected providers 
+    // even after there is a refresh on the page.
     if (JSON.parse(localStorage.getItem('selectedProviders')) || JSON.parse(localStorage.getItem('unSelectedProviders'))) {
       this.selectedProviders = JSON.parse(localStorage.getItem('selectedProviders'));
       this.unselectedProviders = JSON.parse(localStorage.getItem('unSelectedProviders'));
+      // The below condition is to set the default values of unselected and selected providers to 
+      // empty array if selected or unselcted values are not present in the localstorage.
+      // If this condition is not used the html will break as the value will be null in localStorage
+      // if no values are present in it.
       if (!this.selectedProviders) {
         this.selectedProviders = [];
       } else if (!this.unselectedProviders) {
@@ -43,6 +52,11 @@ export class ListComponent implements OnInit {
     }
   }
 
+  // In the below functions toSelectedProviders and toUnSelectedProviders I am pushing the values
+  // selected from selected to unselected and vice versa depedning on the user selection. 
+  // After pushing the values into the respective arrays I am setting these updated array values
+  // into the respective localstorage. From Html i am sending the row value on which the user
+  // selected and performing the actions.
   toSelectedProviders(row) {
     this.selectedProviders.push(row);
     for (var i = 0; i < this.unselectedProviders.length; i++) {
@@ -51,9 +65,8 @@ export class ListComponent implements OnInit {
       }
     }
     window.localStorage.setItem('selectedProviders', JSON.stringify(this.selectedProviders));
-    window.localStorage.setItem('unSelectedProviders',JSON.stringify(this.unselectedProviders));
+    window.localStorage.setItem('unSelectedProviders', JSON.stringify(this.unselectedProviders));
   }
-
   toUnSelectedProviders(row) {
     this.unselectedProviders.push(row);
     for (var i = 0; i < this.selectedProviders.length; i++) {
@@ -62,6 +75,6 @@ export class ListComponent implements OnInit {
       }
     }
     window.localStorage.setItem('unSelectedProviders', JSON.stringify(this.unselectedProviders));
-    window.localStorage.setItem('selectedProviders',JSON.stringify(this.selectedProviders));
+    window.localStorage.setItem('selectedProviders', JSON.stringify(this.selectedProviders));
   }
 }
